4daee822cbd027b65c41d2c2a246a3cfbedb26a3
trujilml <mickeytrj@gmail.com>
2021-11-30 10:51:30 -0600

    connected to mysql2 database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: '559200',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //default response for any other request (not found)
 app.use((req, res) => {
