22b302f0a8fe056ef55f95f5d188821b77bfcfb6
trujilml <mickeytrj@gmail.com>
2021-12-02 14:10:03 -0600

    cleaned up the server js file by moving the candidates and party route in the routes file

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: '559200',
+        database: 'election'
+    });
+
+module.exports = db;
