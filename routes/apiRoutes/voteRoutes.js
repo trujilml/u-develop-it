dc3f4019bd3ec2becbd7fdb461973df596d33728
trujilml <mickeytrj@gmail.com>
2021-12-04 00:21:00 -0600

    added get route for votes - localhost site is working well

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -27,4 +27,24 @@ router.post('/vote', ({ body }, res) => {
     });
 });
 
+router.get('/votes', (req, res) => {
+
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+    FROM votes
+    LEFT JOIN candidates ON votes.candidate_id = candidates.id
+    LEFT JOIN parties ON candidates.party_id = parties.id
+    GROUP BY candidate_id ORDER BY count DESC;`
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 module.exports = router;
